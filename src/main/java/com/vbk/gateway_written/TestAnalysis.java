6ed2650928bcf27b888b3906f6adbb6c07987bee
GarondEisenfaust <chr.schr@web.de>
2021-01-13 17:42:33 +0100

    Added test method that hopefully smells

---
+++ src/main/java/com/vbk/gateway_written/TestAnalysis.java
@@ -0,0 +1,9 @@
+package com.vbk.gateway_written;
+
+public class TestAnalysis {
+  public void unusedMethod(int testInt) {
+    while (true) {
+      System.out.println(testInt + "I am running forever!");
+    }
+  }
+}
