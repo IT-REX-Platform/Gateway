290074497029faa46cb199f11162d52dc790f3e6
GarondEisenfaust <chr.schr@web.de>
2021-01-13 18:41:47 +0100

    Made constructor private

--- src/main/java/de/uni_stuttgart/it_rex/backend_written/TestAnalysis.java
+++ src/main/java/de/uni_stuttgart/it_rex/backend_written/TestAnalysis.java
@@ -2,8 +2,12 @@ package de.uni_stuttgart.it_rex.backend_written;
 
 public class TestAnalysis {
 
+  private TestAnalysis() {
+  }
+
   /**
    * Adds two integers.
+   *
    * @param a first
    * @param b second
    * @return sum
